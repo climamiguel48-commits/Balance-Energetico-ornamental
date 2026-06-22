bc99959d8207bbeeb428af26187fc46afcb0feb4
climamiguel48-commits <climamiguel48@gmail.com>
2026-06-22 11:18:37 -0400

    Actualiza sitio Quarto

--- pumahuida_quarto_website/render_sitio.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d %~dp0
-quarto render
-pause
--- pumahuida_quarto_website/scripts/instalar_paquetes.R
+++
@@ -1,8 +0,0 @@
-# Ejecutar una sola vez si falta algún paquete
-paquetes <- c(
-  "readxl", "dplyr", "lubridate", "readr", "stringr", "purrr",
-  "ggplot2", "plotly", "DT", "tidyr", "tibble"
-)
-instalados <- rownames(installed.packages())
-faltantes <- setdiff(paquetes, instalados)
-if (length(faltantes) > 0) install.packages(faltantes)
